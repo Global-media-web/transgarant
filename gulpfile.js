4f8a7f9fb25e53a385ae29ece6b307307ac0dc5c
Konstantin Gorshkov <master@globalmedia.kz>
2019-11-01 11:23:12 +0600

    Added task processing css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 const gulp = require('gulp'),
       fs = require('fs'),
       pug = require('gulp-pug'),
+      postCSS = require('gulp-postcss'),
+      cssNext = require('postcss-cssnext'),
+      cssnano = require('cssnano'),
+      cssImport = require('postcss-import'),
       browserSync = require('browser-sync').create();
 
 const path = {
     src: {
         pug: "./src/views/*.pug",
+        css: "./src/css/*.css",
     },
     output: {
         pug: "./build/",
+        css: "./build/css/",
     },
     watch: {
         pug: "./src/views/**/*.pug",
+        css: "./src/css/**/*.css",
     }
 }
 
@@ -25,11 +32,23 @@ gulp.task('pug', () =>
         .pipe(browserSync.stream())
 );
 
+gulp.task('css', () =>
+    gulp.src(path.src.css)
+        .pipe(postCSS([
+            cssImport({root: './src/css/*.css'}),
+            cssNext(),
+            cssnano()
+        ]))
+        .pipe(gulp.dest(path.output.css))
+        .pipe(browserSync.stream())
+);
+
 gulp.task('serve', () => {
     browserSync.init({
         server: './build/'
     });
-    gulp.watch(path.watch.pug, gulp.parallel('pug'))
+    gulp.watch(path.watch.css, gulp.parallel('css'));
+    gulp.watch(path.watch.pug, gulp.parallel('pug'));
 });
 
-gulp.task('default', gulp.series('pug', gulp.parallel('serve')));
+gulp.task('default', gulp.series(gulp.parallel('pug', 'css'), gulp.parallel('serve')));
