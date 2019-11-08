6ab158c8f7f01c038d4987bf360947445eeea157
Konstantin Gorshkov <master@globalmedia.kz>
2019-11-08 16:17:30 +0600

    Added production mode to 'css' task

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -4,15 +4,22 @@ const gulp = require('gulp'),
       cssnano = require('cssnano'),
       cssImport = require('postcss-import'),
       browserSync = require('browser-sync'),
+      yargs = require('yargs'),
       {paths} = require('../gulpfile');
+    
+
+const plugins = [
+    cssImport({root: './src/css/*.css'}),
+    postCSSPresetEnv({ stage: 0 })
+];
+
+if(yargs.argv.mode === 'production') {
+    plugins.push(cssnano())
+}
 
 gulp.task('css', () =>
     gulp.src(paths.src.css)
-        .pipe(postCSS([
-            cssImport({root: './src/css/*.css'}),
-            postCSSPresetEnv({ stage: 0 }),
-            cssnano()
-        ]))
+        .pipe(postCSS(plugins))
         .pipe(gulp.dest(paths.output.css))
         .pipe(browserSync.stream())
 );
