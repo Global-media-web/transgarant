816afd573c65352d1f553c8cecaf3443c8d87dd1
Konstantin Gorshkov <master@globalmedia.kz>
2019-11-08 16:07:42 +0600

    Added production mode to 'pug' task

--- gulp-tasks/pug.js
+++ gulp-tasks/pug.js
@@ -2,7 +2,12 @@ const gulp = require('gulp'),
       pug = require('gulp-pug'),
       browserSync = require('browser-sync'),
       fs = require('fs'),
+      yargs = require('yargs'),
+      gulpIf = require('gulp-if'),
+      replace = require('gulp-replace'),
       {paths} = require('../gulpfile');
+
+const isProductionMode = yargs.argv.mode === 'production';
 
 gulp.task('pug', () => 
     gulp.src(paths.src.pug)
@@ -10,6 +15,8 @@ gulp.task('pug', () =>
             pretty: true,
             locals: JSON.parse(fs.readFileSync('./content.json', 'utf-8'))
         }))
+        .pipe(gulpIf(isProductionMode, replace(/(\/js\/.+)(\.js)/g, (match, p1, p2) => `${p1}.min${p2}`)))
+        .pipe(gulpIf(isProductionMode, replace(/(\/css\/.+)(\.css)/g, (match, p1, p2) => `${p1}.min${p2}`)))
         .pipe(gulp.dest(paths.output.pug))
         .pipe(browserSync.stream())
 );
