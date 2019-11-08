74e1b35da8da6d91ed1dd42dcf05fb85fc3a62cf
Konstantin Gorshkov <master@globalmedia.kz>
2019-11-08 16:32:05 +0600

    Added production mode to 'image' task

--- gulp-tasks/image.js
+++ gulp-tasks/image.js
@@ -2,13 +2,19 @@ const gulp = require('gulp'),
       browserSync = require('browser-sync'),
       imagemin = require('gulp-imagemin'),
       mozjpeg = require('imagemin-mozjpeg'),
+      yargs = require('yargs'),
+      gulpIf = require('gulp-if'),
       {paths} = require('../gulpfile');
+
+const isProductionMode = yargs.argv.mode === 'production';
 
 gulp.task('image', () =>
     gulp.src(paths.src.img)
-        .pipe(imagemin([
-            mozjpeg({progressive: true, quality: 75})
-        ]))
+        .pipe(
+            gulpIf(isProductionMode, 
+                imagemin([mozjpeg({progressive: true, quality: 75})])
+            )
+        )
         .pipe(gulp.dest(paths.output.img))
         .pipe(browserSync.stream())
 );
