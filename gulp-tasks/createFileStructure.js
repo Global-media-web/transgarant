301a0f44c91fa4bf7f9341a070e71e37960db0ea
Konstantin Gorshkov <master@globalmedia.kz>
2019-11-11 10:39:54 +0600

    Added 'createFileStructure' task

---
+++ gulp-tasks/createFileStructure.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp'),
+      fs = require('fs');
+
+gulp.task('createFileStructure', (done) => {
+    mkDirIfNotExist('src');
+    mkDirIfNotExist('src/css');
+    mkDirIfNotExist('src/favicon');
+    mkDirIfNotExist('src/img');
+    mkDirIfNotExist('src/js');
+    mkDirIfNotExist('src/views');
+    mkDirIfNotExist('src/fonts');
+    fs.writeFileSync('src/views/content.json', '{}');
+    done();
+})
+
+const mkDirIfNotExist = (dirname) => {
+    try {
+        fs.mkdirSync(dirname);
+    } catch(e) {
+        if(e.code === 'EEXIST') console.log(e.path, 'already exists');
+    }
+}
